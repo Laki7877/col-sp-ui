132c44dda3609a69eee442e541f9bef5b1a91641
Tanapol Nearunchorn <tanapol.sh@gmail.com>
2015-12-17 18:42:28 +0700

    [add product] correct tab menu

--- templates/add_product.php
+++ templates/add_product.php
@@ -1,11 +1,21 @@
-<?php $this->layout('layouts/page-with-sidebar', ['title' => 'User Profile']) ?>
+<?php
+$menus = [
+	["id" => "information", "name" => 'Information', "class" => "active require"],
+	["id" => "images", "name" => 'Images', "class" => "require"],
+	["id" => "category", "name" => 'Category'],
+	["id" => "variation", "name" => 'Variation'],
+	["id" => "more_option", "name" => 'More Options'],
+];
+
+$this->layout('layouts/page-with-sidebar', ['title' => 'User Profile'])
+?>
 
 <?php $this->start('page-body') ?>
 	<div>
 		<? $this->insert('components/page-title', ['text' => "Products/Add Product"]) ?>
-		
+
 		<div class="add-product-body">
-			<? $this->insert('components/tab-nav') ?>
+			<? $this->insert('components/tab-nav', ["items" => $menus]) ?>
 		</div>
 	</div>
 <?php $this->stop() ?>
