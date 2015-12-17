4d07a19aa0654a1588d8d52b7b8e5c406fefed87
Tanapol Nearunchorn <tanapol.sh@gmail.com>
2015-12-17 18:32:23 +0700

    [add product] add require dot on tab nav

--- templates/components/tab-nav.php
+++ templates/components/tab-nav.php
@@ -1,6 +1,6 @@
 <ul class="nav nav-tabs" role="tablist">
-	<li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
-	<li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+	<li role="presentation" class="active require"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+	<li role="presentation" class="require"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
 	<li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
 	<li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
 </ul>
