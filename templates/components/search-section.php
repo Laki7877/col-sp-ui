26a9c7f2e22e3ca472f7a5f2046f2f994632926d
fangbinc <pam.pipuch@gmail.com>
2015-12-18 12:37:44 +0700

    Create new component:search-section in page body

---
+++ templates/components/search-section.php
@@ -0,0 +1,33 @@
+<div class="row search-section">
+  <div class="col-xs-3">
+    <div class="input-group">
+      <div class="input-group-btn">
+        <button type="button" class="body-dropdown-button btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 
+            <span class="dropdown-text margin-right-10">- Choosen Action -</span>
+            <span class="caret margin-left-10"></span>
+        </button>
+        <ul class="dropdown-menu">
+          <li><a href="#">Action</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li><a href="#">Separated link</a></li>
+        </ul>
+      </div><!-- /btn-group -->
+      <button type="button" class="button-white btn btn-default">
+        <span class="button-text-blue">Confirm</span>
+      </button>
+    </div>
+  </div>
+  <div class="col-xs-6">
+    <div class="input-group">
+      <input type="text" class="form-control search-box" placeholder="Recipient's username" aria-describedby="basic-addon2">
+      <button class="input-group-addon search-addon" id="basic-addon2">Search</button>
+    </div>
+  </div>
+  <div class="col-xs-3 checkbox-wrapper">
+    <input type="checkbox" name="checkboxG1" id="checkboxG1" class="css-checkbox" />
+    <label for="checkboxG1" class="css-label">
+        Show Online/Offline Status
+    </label>
+  </div>
+</div>
