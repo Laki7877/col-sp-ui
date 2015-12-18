18e135102d11109dd7e3da4e45c428a4fb48e5e6
Tanapol Nearunchorn <tanapol.sh@gmail.com>
2015-12-18 16:22:39 +0700

    [add product] fix form text vertical position must align at middle

--- templates/partials/add-product-category.php
+++ templates/partials/add-product-category.php
@@ -10,12 +10,12 @@
 				<div class="form-section-header"><h2>Global Category</h2></div>
 				<div class="form-section-content">
 					<div class="form-group">
-						<div class="width-label"><label>Global Category:</label></div>
-						<div class="width-field-normal"><span>Phone Accessory</span></div>
+						<div class="width-label"><label class="control-label">Global Category:</label></div>
+						<div class="width-field-normal"><span class="form-text">Phone Accessory</span></div>
 					</div>
 					<div class="form-group">
-						<div class="width-label"><label>Alternative:</label></div>
-						<div class="width-field-normal"><a class="like-text"><i class="fa fa-plus-circle color-theme"></i> Add Category</a></div>
+						<div class="width-label"><label class="control-label">Alternative:</label></div>
+						<div class="width-field-normal"><a class="like-text form-text"><i class="fa fa-plus-circle color-theme"></i> Add Category</a></div>
 					</div>
 				</div>
 			</div>
@@ -32,8 +32,8 @@
 				<div class="form-section-header"><h2>Local Category</h2></div>
 				<div class="form-section-content">
 					<div class="form-group">
-						<div class="width-label"><label>Local Category:</label></div>
-						<div class="width-field-normal"><a class="like-text"><i class="fa fa-plus-circle color-theme"></i> Add Category</a></div>
+						<div class="width-label"><label class="control-label">Local Category:</label></div>
+						<div class="width-field-normal"><a class="like-text form-text"><i class="fa fa-plus-circle color-theme"></i> Add Category</a></div>
 					</div>
 				</div>
 			</div>
