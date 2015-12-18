96a1f405b347022bce7bbe08ec5421b7c1306593
fangbinc <pam.pipuch@gmail.com>
2015-12-18 13:48:29 +0700

    Add table section in page body

--- templates/index.php
+++ templates/index.php
@@ -14,5 +14,49 @@
         <a class="filter-seperator">Not Approved</a>            
       </div>
     </div>
+    <div class="table-section">
+      <table class="table">
+        <thead>
+          <tr class="table-head">
+            <th class="checkbox-column"></th>
+            <th class="display-column"></th>
+            <th>Product Name</th>
+            <th class="price-column">Price</th>
+            <th>Info.</th>
+            <th>Image</th>
+            <th class="status-column">Status</th>
+            <th class="live-column">Live</th>
+            <th class="visible-column">Visible</th>
+            <th class="modified-column">Modified</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="checkbox-column"></td>
+            <td class="display-column"></td>
+            <td>After all this years</td>
+            <td class="price-column"></td>
+            <td class="info-column"></td>
+            <td class="image-column"></td>
+            <td class="status-column"></td>
+            <td class="live-column"></td>
+            <td class="visible-column"></td>
+            <td class="modified-column"></td>
+          </tr>
+          <tr>
+            <td class="checkbox-column"></td>
+            <td class="display-column"></td>
+            <td>After all this years</td>
+            <td class="price-column"></td>
+            <td class="info-column"></td>
+            <td class="image-column"></td>
+            <td class="status-column"></td>
+            <td class="live-column"></td>
+            <td class="visible-column"></td>
+            <td class="modified-column"></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 	</div>
 <?php $this->stop() ?>
