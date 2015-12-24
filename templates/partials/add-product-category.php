203d2d58fca38cdd3eb26df7e464d58a8b05512d
Tanapol Nearunchorn <tanapol.sh@gmail.com>
2015-12-25 00:20:14 +0700

    [add product] adjust to match new style of category tab

--- templates/partials/add-product-category.php
+++ templates/partials/add-product-category.php
@@ -11,10 +11,14 @@
 						<div class="width-field-normal"><span class="form-text">Phone Accessory</span></div>
 					</div>
 					<div class="form-group">
-						<div class="width-label"><label class="control-label">Alternative:</label></div>
+						<div class="width-label"><label class="control-label">1st Alternative:</label></div>
+						<div class="width-field-normal"><a class="form-text">Electronic Gadget</a><i class="clickable fa fa-trash margin-left-10"></i></div>
+					</div>
+					<div class="form-group">
+						<div class="width-label"><label class="control-label">2nd Alternative:</label></div>
 						<div class="width-field-normal">
 							<a class="like-text form-text" data-toggle="modal" data-target="#global-category">
-								<i class="fa fa-plus-circle color-theme"></i> Add Category
+								<i class="fa fa-plus-circle color-theme"></i> Add Alternative Category
 							</a>
 						</div>
 					</div>
@@ -22,7 +26,7 @@
 			</div>
 		</div>
 		<div class="col-xs-3">
-			<h3>Global Category</h3>
+			<h4>Global Category</h4>
 			<p>Global category is where this product will belong to when user finds it on the market place. You can have one main global category, but you can have an alternative category where the product will be cloned into.</p>
 		</div>
 	</div>
@@ -36,7 +40,23 @@
 						<div class="width-label"><label class="control-label">Local Category:</label></div>
 						<div class="width-field-normal">
 							<a class="like-text form-text" data-toggle="modal" data-target="#local-category">
-								<i class="fa fa-plus-circle color-theme"></i> Add Category
+								<i class="fa fa-plus-circle color-theme"></i> Add Alternative Category
+							</a>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="width-label"><label class="control-label">1st Alternative:</label></div>
+						<div class="width-field-normal">
+							<a class="like-text form-text" data-toggle="modal" data-target="#local-category">
+								<i class="fa fa-plus-circle color-theme"></i> Add Alternative Category
+							</a>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="width-label"><label class="control-label">2st Alternative:</label></div>
+						<div class="width-field-normal">
+							<a class="like-text form-text" data-toggle="modal" data-target="#local-category">
+								<i class="fa fa-plus-circle color-theme"></i> Add Alternative Category
 							</a>
 						</div>
 					</div>
@@ -44,7 +64,7 @@
 			</div>
 		</div>
 		<div class="col-xs-3">
-			<h3>Local Category</h3>
+			<h4>Local Category</h4>
 			<p>Local category is where this product will belong when user finds it on this store. You can have one main local category but many alternative ones.</p>
 		</div>
 	</div>
