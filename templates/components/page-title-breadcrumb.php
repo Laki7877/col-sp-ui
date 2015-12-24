b0e16d2c908e348406399916cb5b43c4d12e8bf7
Tanapol Nearunchorn <tanapol.sh@gmail.com>
2015-12-25 00:38:19 +0700

    fix add product page breadcrumb is using wrong color

--- templates/components/page-title-breadcrumb.php
+++ templates/components/page-title-breadcrumb.php
@@ -1,7 +1,7 @@
 <?php
 $extracted = explode('/', $text);
 $extracted = array_map(function($item, $idx) {
-  return "<span class=\"ah-breadcrumb-path ah-breadcrumb-idx-$idx\">".$item.'</span>';
+  return "<a class=\"ah-breadcrumb-path ah-breadcrumb-idx-$idx\">".$item.'</a>';
 }, $extracted, array_keys($extracted));
 $newText = implode('<span class="ah-breadcrumb-splitter">/</span>', $extracted);
 ?>
