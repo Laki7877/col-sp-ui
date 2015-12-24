e1ef9308770d6ebc5763354e8101b5ac678fcebc
Tanapol Nearunchorn <tanapol.sh@gmail.com>
2015-12-25 00:46:06 +0700

    add page header buttons

--- templates/components/page-title-breadcrumb.php
+++ templates/components/page-title-breadcrumb.php
@@ -7,4 +7,9 @@ $newText = implode('<span class="ah-breadcrumb-splitter">/</span>', $extracted);
 ?>
 <div class="page-header no-padding">
     <h1 class="float-left page-header-title ah-breadcrumb"><?= $newText ?></h1>
+    <div class="page-actions float-right">
+		<button class="btn btn-white btn-width-xl">Preview</button>
+		<button class="btn btn-white btn-width-xl">Save as Draft</button>
+		<button class="btn btn-blue btn-width-xl">Publish</button>
+	</div>
 </div>
