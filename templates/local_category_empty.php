64221d71331be692620c222ca6693e849769da2c
fangbinc <pam.pipuch@gmail.com>
2015-12-25 18:40:13 +0700

    [Local Category] Fix alignment empty page

--- templates/local_category_empty.php
+++ templates/local_category_empty.php
@@ -3,7 +3,7 @@
 <?php $this->start('page-body') ?>
 	<div class="local-category-page">
 		<? $this->insert('components/page-title-with-buttons-local-category', ['text' => 'Local Category']) ?>
-		<div class="local-category-empty-section margin-top-20 row">
+		<div class="local-category-empty-section margin-top-20">
 			<div class="col-xs-2">
 				<div class="zero-category-image">
 				</div>
