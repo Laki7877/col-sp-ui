20203dca5afdcd52e8cc6e42b1d47afa620001c6
fangbinc <pam.pipuch@gmail.com>
2015-12-25 20:44:23 +0700

    [Product] Add template for empty category

---
+++ templates/components/local-category-empty-content.php
@@ -0,0 +1,9 @@
+<div class="local-category-empty-section margin-top-20">
+	<div class="col-xs-2">
+		<div class="zero-category-image">
+		</div>
+	</div>
+	<div class="col-xs-10 local-category-empty-text">
+		<?=$text?>
+	</div>
+</div>
--- templates/local_category_empty.php
+++ templates/local_category_empty.php
@@ -3,15 +3,7 @@
 <?php $this->start('page-body') ?>
 	<div class="local-category-page">
 		<? $this->insert('components/page-title-with-buttons-local-category', ['text' => 'Local Category']) ?>
-		<div class="local-category-empty-section margin-top-20">
-			<div class="col-xs-2">
-				<div class="zero-category-image">
-				</div>
-			</div>
-			<div class="col-xs-10 local-category-empty-text">
-				You do not have local category
-			</div>
-		</div>
+		<? $this->insert('components/local-category-empty-content', ['text' => 'You do not have local category']) ?>			
 	</div>
 
 <?php $this->stop() ?>
