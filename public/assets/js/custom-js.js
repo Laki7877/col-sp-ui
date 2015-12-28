d69638884d51d722a42236641be3a717a06a642c
Tanapol Nearunchorn <tanapol.sh@gmail.com>
2015-12-28 16:36:38 +0700

    improve popover behavior

--- public/assets/js/custom-js.js
+++ public/assets/js/custom-js.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
-	$('[data-toggle="popover"]').popover();
+	$('[data-toggle="popover"]').popover({trigger: 'manual'});
+	$(document).on('click', function(e) {
+		var $target = $(e.target);
+		var isPopover = $target.is('[data-toggle="popover"]');
+
+		if (isPopover) {
+			$target.popover('show');
+		}
+		else {
+			$('[data-toggle="popover"]').popover('hide');
+		}
+	});
 
 	// Logic for add text to dropdown at Product page top filter
 	$(".dropdown-btn .dropdown-menu a").click(function() {
