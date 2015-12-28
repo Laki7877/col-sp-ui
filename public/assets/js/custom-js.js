b5d6aba3e9a92f2e77c552961328ba2e48482799
Tanapol Nearunchorn <tanapol.sh@gmail.com>
2015-12-28 11:06:53 +0700

    add sub-sidebar

--- public/assets/js/custom-js.js
+++ public/assets/js/custom-js.js
@@ -7,4 +7,23 @@ $(document).ready(function () {
 		var $parent = $this.closest('.dropdown-btn');
 		$('.dropdown-text', $parent).text($this.html());     
 	});
+
+
+	$('[data-toggle="sub-sidebar"]').hover(function(e) { //mouse enter
+		var $this = $(this);
+		var $target = $($this.data('target'));
+		$('.sub-sidebar').hide();
+		$target.show();
+	}, function(e) { //mouse leave
+		var $this = $(this);
+		var $target = $($this.data('target'));
+		$target.hide();
+	});
+	$('.sub-sidebar').hover(function(e) { //mouse enter
+		var $this = $(this);
+		$this.show();
+	}, function(e) { //mouse leave
+		var $this = $(this);
+		$this.hide();
+	});
 });
