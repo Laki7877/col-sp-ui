39ed81ad9275a9c0f4f5104e3eb7fb0c9a3fad21
Natt Phenjati <nattphenjati@gmail.com>
2015-12-29 16:16:17 +0700

    Add components for Admin

---
+++ templates/components/page-title-breadcrumb-admin-add-attribute.php
@@ -0,0 +1,16 @@
+<?php
+$extracted = explode('/', $text);
+$extracted = array_map(function($item, $idx) {
+  return "<a class=\"ah-breadcrumb-path ah-breadcrumb-idx-$idx\">".$item.'</a>';
+}, $extracted, array_keys($extracted));
+$newText = implode('<span class="ah-breadcrumb-splitter">/</span>', $extracted);
+?>
+<div class="page-header with-border">
+    <h1 class="float-left page-header-title ah-breadcrumb"><?= $newText ?></h1>
+    <div class="page-actions float-right">
+		<button class="btn btn-white btn-width-xl">Cancel</button>
+		<button class="btn btn-blue btn-width-xl">Save</button>
+	</div>
+</div>
+
+
---
+++ templates/components/page-title-with-buttons-admin-attribute.php
@@ -0,0 +1,8 @@
+<div class="page-header with-border">
+    <h1 class="float-left page-header-title"><?= $text ?></h1>
+    <span class="float-right page-header-action">
+        <button type="button" class="btn-blue btn btn-width-xl">
+          <span class="">Add Attribute</span>
+        </button>
+    </span>
+</div>
