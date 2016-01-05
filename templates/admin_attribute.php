8430046f46bc4fdae2497efca97f7537d3a17aed
Natt Phenjati <nattphenjati@gmail.com>
2016-01-05 09:55:34 +0700

    [General] Add Loading Modal

--- templates/admin_attribute.php
+++ templates/admin_attribute.php
@@ -128,4 +128,24 @@
       </span>
     </div>
 	</div>
+
+  <a data-toggle="modal" data-target="#modal-loading">Loading Modal</a>
+
+    <!-- Modal -->
+  <div class="modal fade" tabindex="-1" role="dialog" id="modal-loading">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-body">
+          <h3 class="modal-title margin-bottom-20">Processing...</h3>
+          <div class="progress margin-0">
+            <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+            </div>
+          </div>
+        </div> <!-- end .modal-body -->
+      </div> <!-- end .modal-content -->
+    </div> <!-- end .modal-dialog -->
+  </div> <!-- end .modal -->
+
+
+ 
 <?php $this->stop() ?>
