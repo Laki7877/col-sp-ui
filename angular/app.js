9221d17c4a01ecc847c4a40f59747faeadd16398
Poon Wu <poon.wuthi@gmail.com>
2016-01-06 17:03:51 +0700

    Renamed files

--- angular/app.js
+++ angular/app.js
@@ -6,9 +6,7 @@ var config = require('./config');
 
 //External dependencies
 require('angular-file-upload');
-require('angular-sanitize');
 require('angular-base64');
-require('ui-select');
 
 //Internal dependencies
 var controllers = bulk.controllers;
@@ -16,7 +14,7 @@ var services = bulk.services;
 var helpers = bulk.helpers;
 var directives = bulk.directives;
 
-var app = angular.module('colspApp', ['ui.select', 'ngSanitize', 'angularFileUpload', 'base64'])
+var app = angular.module('colspApp', ['angularFileUpload', 'base64'])
 
 //App init
 .run(['$base64', 'storage', function($base64, storage) {
@@ -31,12 +29,12 @@ var app = angular.module('colspApp', ['ui.select', 'ngSanitize', 'angularFileUpl
 .factory('storage', helpers.storage)
 .factory('util', helpers.util)
 .factory('base64', helpers.base64)
-.factory('Product', services.products)
-.factory('Image', services.images)
+.factory('Product', services.product)
+.factory('Image', services.image)
 
 //Directives
 .directive('ngDelegate', directives.ngDelegate)
 
 //Controllers
-.controller('ProductListCtrl', controllers.products_list)
-.controller('ProductAddCtrl', controllers.products_add);
+.controller('ProductListCtrl', controllers.productList)
+.controller('ProductAddCtrl', controllers.productAdd);
