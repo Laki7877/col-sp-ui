6b65a0820e9a35542977f51b0e138e2e8a031844
Suppatach Sabpisal <ssabpisa@me.com>
2016-01-06 16:46:57 +0700

    bundle

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -50,17 +50,84 @@ module.exports = {
 },{}],3:[function(require,module,exports){
 module.exports = ['$scope', '$http', 'Product', 'Image', 'FileUploader',  function($scope, $http, Product, Image, FileUploader){
 	//Variation Options Available
-	$scope.variation_options = [{
+	$scope.available_attribute_options = [{
 		name: 'Capacity',
-		value: 0,
-		unit: 'mAh'
+		id: 5,
+		unit: 'mAh',
+		available_options: [
+			{ Id: 51, Name: '1000 mAh'},
+			{ Id: 31, Name: '2500 mAh'},
+			{ Id: 92, Name: '10000 mAh' }
+		]
 	},{
 		name: 'Material',
-		value: 1,
-		unit: ''
+		id: 1,
+		unit: '',
+		available_options: [
+			{ Id: 1, Name: "Palladium" },
+			{ Id: 2, Name: "Carbon Fiber" },
+			{ Id: 3, Name: "Nano Polymer" }
+		]
 	}];
 
-	  //Will Replace or wrap with angular equivalent
+	var Pair = function(a,b){ 
+		this.first = a; 
+		this.second = b; 
+		this.hash = a.Id + "-" + b.Id;
+		this.text = (a.Name + ", " + b.Name)
+	};
+
+	$scope.variants = [];
+	$scope.attribute_options = {
+		0: {
+			attribute: null,
+			options: []
+		},
+		1: {
+			attribute: null,
+			options: []
+		}	
+	};
+
+	//When selected attribute change we have to regen cross multiply
+	$scope.$watch('attribute_options[0].attribute_id', function(oldv, newv){
+		//Reset Options
+		$scope.attribute_options[0].options = [];
+
+	});	
+	$scope.$watch('attribute_options[1].attribute_id', function(oldv, newv){
+		//Reset Options
+		$scope.attribute_options[1].options = [];
+	});	
+	//Multiply attributes into variants
+	$scope.$watch('attribute_options', function(oldv, newv){
+		if($scope.attribute_options[1].options.length == 0) return;
+		if($scope.attribute_options[0].options.length == 0) return;
+	
+		//TODO: Don't clear but only removed changed/stale	
+		$scope.variants = [];
+		$scope.attribute_options[0].options.forEach(function(A){
+			$scope.attribute_options[1].options.forEach(function(B){
+				$scope.variants.push(new Pair(A,B));
+			});
+		});
+		
+		console.log($scope.variants);
+	}, true);
+
+	//When selected attribute change, the other box wil not allow to have selected option
+
+	//Initialize Select2 (variation select)
+	$.fn.select2.defaults.set("tokenSeparators", [",", " "]);
+	$(document).on('shown.bs.tab ready', function(){
+		$(".select2-init").select2();
+		$(".select2-init").on("change", function(ev){
+			$scope.$digest();
+		});
+	});
+
+
+	//TODO: Init CK Editor (apparently this breaks)
 /*	$('[ckeditor-initialize]').each(function(idx, textarea) {
 		CKEDITOR.replace( textarea );
 	});
@@ -68,12 +135,6 @@ module.exports = ['$scope', '$http', 'Product', 'Image', 'FileUploader',  functi
 		format: "LL" // this is momentjs format make it show only date, no time will be show. see: http://momentjs.com/docs/#/displaying/format/
 	});
 */
-	//Initialize Select2
-	$.fn.select2.defaults.set("tokenSeparators", [",", " "]);
-	$(document).on('shown.bs.tab ready', function(){
-			$(".select2-init").select2();
-	});
-
 	$("body").tooltip({ selector: '[data-toggle=tooltip]' });
 
 	
