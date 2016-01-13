09d26be644c31fc24ed2d0fd5d6ad0841d325c8e
Natt Phenjati <nattphenjati@gmail.com>
2016-01-13 17:18:02 +0700

    Make a simple 404 page

--- templates/local_category_empty.php
+++ templates/local_category_empty.php
@@ -1,4 +1,4 @@
-<?php $this->layout('layouts/page-with-sidebar', ['title' => 'Local Category']) ?>
+<?php $this->layout('layouts/page-with-sidebar', ['title' => '404']) ?>
 
 <?php $this->start('page-body') ?>
 	<div class="local-category-page">
