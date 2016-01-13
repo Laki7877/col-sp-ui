ad8c1d0c78d9c83461bdd05477f2f5b5d4ca84c4
fangbinc <pam.pipuch@gmail.com>
2016-01-13 18:14:46 +0700

    [Component] Add componet text with label

---
+++ templates/components/forms/text-with-label.php
@@ -0,0 +1,6 @@
+<div class="form-group <?=$form_group_class?>">
+	<div class="width-label"><label class="control-label <?=$label_class?>"><?= $label ?></label></div>
+    <div class="col-xs-10">
+      <p class="form-control-static"><?= $field_content ?></p>
+    </div>
+</div>
