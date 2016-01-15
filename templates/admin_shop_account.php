91234e120b930720b7d9de3af36e1ba714702029
fangbinc <pam.pipuch@gmail.com>
2016-01-15 14:00:56 +0700

    [Fix] fix link in Shop Account page

--- templates/admin_shop_account.php
+++ templates/admin_shop_account.php
@@ -2,7 +2,7 @@
 
 <?php $this->start('page-body') ?>
   <div>
-    <? $this->insert('components/page-title-with-one-button', ['text' => 'Shop Accounts','button' => 'Create New Seller Shop', 'button_class' => 'btn-width-xxxl', 'link' => '?p=admin_add_attribute']) ?>
+    <? $this->insert('components/page-title-with-one-button', ['text' => 'Shop Accounts','button' => 'Create New Seller Shop', 'button_class' => 'btn-width-xxxl', 'link' => '?p=admin_add_shop_account']) ?>
     <? $this->insert('components/search-section-admin-attribute', ['searchPlaceHolder' => 'Search for Admin Accounts']) ?>
     <div class="table-section">
       <table class="table table-curved table-border-none">
