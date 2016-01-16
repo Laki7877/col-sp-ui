6bad78cc967ff62343c42aefd9493d63cb7a0f5d
poonwu <poon.wuthi@gmail.com>
2016-01-17 01:53:44 +0700

    Refixed naming bug

--- templates/admin_add_attribute_set.php
+++ templates/admin_add_attribute_set.php
@@ -2,7 +2,7 @@
 
 <?php $this->start('page-body') ?>
 	<div ng-controller="AdminAttributeSetAddCtrl" ng-init="init(<?=$params?>)">
-    <? $this->insert('components/page-title-breadcrumb-with-cancel-save', ['text' => "Attribute/" . $title, 'urls' => ['/admin/attributesets'], 'class' =>'{disabled : form.$invalid}']) ?>
+    <? $this->insert('components/page-title-breadcrumb-with-cancel-save', ['text' => "Attribute Set/" . $title, 'urls' => ['/admin/attributesets'], 'class' =>'{disabled : form.$invalid}']) ?>
     <div ng-show="alert.show" uib-alert template-url="common/alert" type="{{ alert.type }}" close="alert.close()">{{alert.message}}</div>
     <form class="ah-form sticky-mainform-action margin-top-30" name="form">
       <div class="row">
