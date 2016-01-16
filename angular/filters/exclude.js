d4ff74ee8cc0f8315574a49324c2c453bf0ffef7
Suppatach Sabpisal <ssabpisa@me.com>
2016-01-16 19:49:37 +0700

    Prevent double choosing category

--- angular/filters/exclude.js
+++ angular/filters/exclude.js
@@ -4,7 +4,23 @@ module.exports = [function() {
   return function(arr, other, trackBy) {
 
      return arr.filter(function(elem){
-        return elem[trackBy] != (other[trackBy] || null);
+     	console.log("comparing", elem, other);
+        if(other instanceof Array){
+        	//throw away if elem matches any of the given other list
+        	var p = true;
+        	for(var j = 0; j < other.length; j++){
+        		var k = (elem[trackBy] == ((other[j] || {trackBy: null} )[trackBy] || null));
+        		//matched atleast one, stop and throw away
+        		if(k){
+        			p = false;
+        			break;
+        		}
+        	}
+        	return p;
+        }else{
+        	//return true to keep
+        	return elem[trackBy] != (other[trackBy] || null);
+        }
      });
   }
 }];
