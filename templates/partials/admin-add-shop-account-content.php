491f4ce71be6194b4d1b761d44b6e5f9a0f0f37f
fangbinc <pam.pipuch@gmail.com>
2016-01-17 13:23:47 +0700

    [Admin] Add shop page: add table Shop Users

--- templates/partials/admin-add-shop-account-content.php
+++ templates/partials/admin-add-shop-account-content.php
@@ -26,7 +26,7 @@
 	<div class="row">
 		<div class="col-xs-12">
 			<div class="form-section">
-				<div class="form-section-header"><h2>Admin Account Roles</h2></div>
+				<div class="form-section-header"><h2>Shop Owner Information</h2></div>
 				<div class="form-section-content">
 					<? $this->insert('components/forms/input-text-with-label', ["label" => "Name", "label_class" => "required"]) ?>
 					<? $this->insert('components/forms/input-text-with-label', ["label" => "Position", "label_class" => "required"]) ?>
@@ -35,6 +35,50 @@
 					<? $this->insert('components/forms/password-field', ["label" => "Password", "label_class" => "required"]) ?>
 					<? $this->insert('components/forms/password-field', ["label" => "Confirm Password", "label_class" => "required"]) ?>
 
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="form-section">
+				<div class="form-section-header"><h2>Shop Users</h2></div>
+				<div class="form-section-content">
+					<div class="form-group">
+						<!-- <table class="table table-hover">
+						  ...
+						</table> -->
+						<table class="table table-hover no-margin"> 
+							<thead> 
+								<tr> 
+									<th>ID</th> 
+									<th>Name</th> 
+									<th>Email</th> 
+									<th>Role</th> 
+									<th>Status</th> 
+									<th class="text-align-center">Action</th> 
+								</tr> 
+							</thead> 
+							<tbody> 
+								<tr> 
+									<td>001</td> 
+									<td>Palmy Greengreengreen</td> 
+									<td>user1@lara.co.th</td>
+									<td>Shop Owner</td>
+									<td>Active</td>
+									<td class="text-align-center"><a class="btn btn-white btn-width-xl" href="?p=">Login-As</a></td> 
+								</tr> 
+								<tr> 
+									<td>002</td> 
+									<td>Sukrit Striketwo</td> 
+									<td>content@lara.co.th</td>
+									<td>Content Team</td>
+									<td>Inactive</td>
+									<td class="text-align-center"><a class="btn btn-white btn-width-xl" href="?p=">Login-As</a></td> 
+								</tr> 
+							</tbody> 
+						</table>
+					</div>
 				</div>
 			</div>
 		</div>
