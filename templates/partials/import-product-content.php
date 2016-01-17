0a7fefd6ad9e1d73ac74f4b12c95b94a108b04ff
fangbinc <pam.pipuch@gmail.com>
2016-01-17 17:52:03 +0700

    [Product] Add product import product page

---
+++ templates/partials/import-product-content.php
@@ -0,0 +1,26 @@
+<div id="import-product-content-page">
+	
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="form-section">
+				<div class="form-section-header"><h2>Getting Started</h2></div>
+				<div class="form-section-content">
+					<? $this->insert('components/forms/button-with-label', ["label" => "Import Product Template", "size" => "large", "btnClass" => "btn-white", "buttonText" => "Download Template (.xls)"]) ?>					
+					<? $this->insert('components/forms/button-with-label', ["label" => "Category ID List", "size" => "large", "btnClass" => "btn-white", "buttonText" => "Download Category ID (.xls)"]) ?>					
+					<? $this->insert('components/forms/button-with-label', ["label" => "Guideline", "size" => "large", "btnClass" => "btn-white", "buttonText" => "Download Guideline (.pdf)"]) ?>					
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="form-section">
+				<div class="form-section-header"><h2>Upload File</h2></div>
+				<div class="form-section-content">
+					<? $this->insert('components/forms/password-field', ["label" => "Choose File"]) ?>
+					<? $this->insert('components/forms/button-with-label', ["buttonText" => "Import", "btnClass" => "btn-blue"]) ?>					
+				</div>
+			</div>
+		</div>
+	</div>	
+</div>
