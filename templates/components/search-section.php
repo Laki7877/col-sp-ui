67588dad918b6375bb3c0d9d8cf022d48256b8ba
Tanapol Nearunchorn <tanapol.sh@gmail.com>
2016-01-19 21:37:49 +0700

    make search section customizable

--- templates/components/search-section.php
+++ templates/components/search-section.php
@@ -1,3 +1,11 @@
+<?php
+if (!isset($actions)) {
+  $actions = ['Action', 'Another action'];
+}
+if (!isset($serach_placeholder)) {
+  $serach_placeholder = "Search for Product SKU, Name, ...";
+}
+?>
 <div class="row search-section-wrapper">
   <div class="search-section section-action">
     <div class="input-group">
@@ -8,10 +16,9 @@
               <span class="caret margin-left-10"></span>
           </button>
           <ul class="dropdown-menu search-product-dropdown">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li><a href="#">Separated link</a></li>
+            <? foreach ($actions as $action): ?>
+              <li><a href="#"><?= $action ?></a></li>
+            <? endforeach ?>
           </ul>
         </div>
       </div><!-- /btn-group -->
@@ -24,7 +31,7 @@
   </div>
   <div class="search-section section-search">
     <div class="input-group">
-      <input type="text" class="form-control input-search-icon search-box" placeholder="Search for Product SKU, Name, ..." aria-describedby="basic-addon2">
+      <input type="text" class="form-control input-search-icon search-box" placeholder="<?= $serach_placeholder ?>" aria-describedby="basic-addon2">
       <span class="input-group-btn">
         <button class="btn btn-white" type="button">Search</button>
       </span>
@@ -32,8 +39,6 @@
   </div>
   <div class="search-section section-filter checkbox-wrapper <?=$optional_class?>">
     <input type="checkbox" name="checkboxG1" id="checkboxG1" class="css-checkbox" />
-    <label for="checkboxG1" class="css-label">
-        Show Online/Offline Status
-    </label>
+    <label for="checkboxG1" class="css-label">Show Online/Offline Status</label>
   </div>
 </div>
