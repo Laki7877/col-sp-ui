77fc3f2b8b94e59c367c98f7f6b9a685468e7b8b
fangbinc <pam.pipuch@gmail.com>
2016-01-19 11:24:12 +0700

    [Product] add export progress modal

---
+++ templates/components/modal-export-product-progressing.php
@@ -0,0 +1,27 @@
+<?
+$progress = "0";
+if (isset($percent)) $progress = $percent;
+?>
+
+<div class="modal fade" tabindex="-1" role="dialog" id="<?= $id ?>">
+	<div class="modal-dialog modal-size-warning">
+		<div class="modal-content">
+			<div class="modal-body confirmation-modal no-margin">
+				<div class="row">
+					<div class="col-xs-12 margin-bottom-30">
+						<h2 class="font-size-20 text-centerx text-normal margin-bottom-20">Exporting...</h2>
+						<div class="progress">
+						  <div class="progress-bar" role="progressbar" aria-valuenow="<?=$progress?>" aria-valuemin="0" aria-valuemax="100" style="width: <?=$progress?>%;">
+						    <span class="sr-only"><?=$progress?>% Complete</span>
+						  </div>
+						</div>
+					</div>
+					<div class="confirmation-action no-margin">
+						<button class="btn btn-white" data-dismiss="modal">Cancel</button>
+						<button type="button" disabled class="btn btn-grey">Download</button>
+					</div> <!-- end .col-xs-12 -->
+				</div> <!-- end .row -->
+			</div> <!-- end .modal-body -->
+		</div> <!-- end .modal-content -->
+	</div> <!-- end .modal-dialog -->
+</div> <!-- end .modal -->
--- templates/layouts/page-with-sidebar.php
+++ templates/layouts/page-with-sidebar.php
@@ -12,13 +12,14 @@
         <li class="item active margin-top-20"><a href="">View</a></li>
         <li class="item"><a href="?p=add_product">Add</a></li>
         <li class="item"><a href="?p=import_product">Import</a></li>
-        <li class="item"><a href="" data-toggle="modal" data-target="#export-product">Export</a></li>
+        <li class="item"><a href="" data-toggle="modal" data-target="#export-product-progressing">Export</a></li>
         <li class="item margin-top-30"><a href="">Local Category</a></li>
         <li class="item"><a href="">Product Review</a></li>
         <li class="item"><a href="">Product Images</a></li>
     </ul>
 
     <? $this->insert('components/modal-export-product', ['id' => 'export-product', 'newProductNum' => '1,500']) ?>
+    <? $this->insert('components/modal-export-product-progressing', ['id' => 'export-product-progressing', 'percent' => '60']) ?>
 
 <?php $this->stop() ?>
 
