b76de211f75f69770356ad7f926f014ab15c3e50
Tanapol Nearunchorn <tanapol.sh@gmail.com>
2016-01-19 20:52:26 +0700

    adjust admin-add-account form to match new UI

--- templates/components/forms/text-with-label.php
+++ templates/components/forms/text-with-label.php
@@ -1,6 +1,6 @@
 <div class="form-group <?=$form_group_class?>">
 	<div class="width-label"><label class="control-label <?=$label_class?>"><?= $label ?></label></div>
-    <div class="col-xs-10">
+    <div class="width-field-xxl">
       <p class="form-control-static"><?= $field_content ?></p>
     </div>
 </div>
--- templates/partials/admin-add-account-content.php
+++ templates/partials/admin-add-account-content.php
@@ -5,10 +5,14 @@
 			<div class="form-section">
 				<div class="form-section-header"><h2>Admin Account Information</h2></div>
 				<div class="form-section-content">
-					<? $this->insert('components/forms/text-with-label', ["label" => "Admin ID", "field_content" => "001"]) ?>					
+					<? $this->insert('components/forms/text-with-label', ["label" => "Admin ID", "field_content" => "001"]) ?>
 					<? $this->insert('components/forms/input-text-with-label', ["label" => "Email", "label_class" => "required"]) ?>
 					<? $this->insert('components/forms/input-text-with-label', ["label" => "Name", "label_class" => "required"]) ?>
-					<? $this->insert('components/forms/password-field', ["label" => "Password", "label_class" => "required"]) ?>
+					<? $this->insert('components/forms/input-text-with-label', ["label" => "Phone Number", "label_class" => "required"]) ?>
+					<? $this->insert('components/forms/input-text-with-label', ["label" => "Employee ID"]) ?>
+					<? $this->insert('components/forms/input-text-with-label', ["label" => "Position"]) ?>
+					<? $this->insert('components/forms/input-text-with-label', ["label" => "Division / BU"]) ?>
+					<? $this->insert('components/forms/password-field', ["label" => "Password", "label_class" => "required", 'form_group_class' => 'margin-top-30']) ?>
 					<? $this->insert('components/forms/password-field', ["label" => "Confirm Password", "label_class" => "required"]) ?>
 				</div>
 			</div>
@@ -19,14 +23,11 @@
 			<div class="form-section">
 				<div class="form-section-header"><h2>Admin Account Roles</h2></div>
 				<div class="form-section-content">
-					<? $this->insert('components/forms/dropdown-with-label', ["label" => "Admin Roles", "label_class" => "required", "options" => ["Super Admin", "User"]]) ?>
+					<? $this->insert('components/forms/dropdown-with-label', ["label" => "Admin Role", "label_class" => "required", "options" => ["Super Admin", "User"]]) ?>
 					<div class="form-group">
 						<div class="width-label"></div>
 						<div class="width-field-normal">
-							<a class="like-text form-text">
-								<i class="fa fa-plus-circle color-theme"></i>
-								Add New Admin Role
-							</a>
+							<a href="/?p=admin_add_role" class="form-text">Create New Admin Role</a>
 						</div>
 					</div>
 				</div>
