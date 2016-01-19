2284b08462c0bc90a6440b5f362c6cba9667fd92
fangbinc <pam.pipuch@gmail.com>
2016-01-19 10:40:17 +0700

    [Component] fix button for adding modal

--- templates/components/forms/button-with-label.php
+++ templates/components/forms/button-with-label.php
@@ -5,7 +5,7 @@ if (isset($size)) $inputSize = "button-size-$size";
 <div class="form-group <?=$form_group_class?>">
 	<div class="width-label"><label class="control-label"><?= $label ?></label></div>
 	<div class="<?= $inputSize ?>">
-		<a class="<?= $inputSize ?> btn <?= $btnClass ?> btn-width-xl" href="#"><?= $buttonText?></a>
+		<a class="<?= $inputSize ?> btn <?= $btnClass ?> btn-width-xl" <?=$modalData?> ><?= $buttonText?></a>
 	</div>
 	<? if (!empty($tooltip)): ?>
 		<div class="width-field-tooltip no-padding-left"><i class="fa fa-2x fa-question-circle color-grey" data-toggle="btooltip" data-placement="right" title="<?= $tooltip ?>"></i></div>
