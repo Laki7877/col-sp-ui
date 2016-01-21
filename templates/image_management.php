806deb5c67a88aba54bc05656988a55d4f48efe7
fangbinc <pam.pipuch@gmail.com>
2016-01-21 18:34:06 +0700

    [Image] Add footer

--- templates/image_management.php
+++ templates/image_management.php
@@ -103,7 +103,42 @@
         </tbody>
       </table>
     </div>
+    <div class="page-navigation">
+      <span>
+        <i class="fa fa-chevron-left grey-chevron"></i>
+        <span> Page 1 of 1</span>
+        <i class="fa fa-chevron-right padding-right-15 blue-chevron"></i>
+        <span class="view-page-separator">View per page</span>
+        <!-- Split button -->
+        <div class="btn-group dropdown-btn">
+          <button type="button" class="btn btn-default dropdown-text">5</button>
+          <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span class="caret"></span>
+            <span class="sr-only">Toggle Dropdown</span>
+          </button>
+          <ul class="dropdown-menu dropdown-menu-right">
+            <li><a href="#">5</a></li>
+            <li><a href="#">10</a></li>
+            <li><a href="#">15</a></li>
+          </ul>
+        </div>
+      </span>
+    </div>
   </div>
+  <div>
+    <form class="ah-form sticky-mainform-action">
+      <div class="add-product-form-action main-form-action full-width-row">
+        <div class="container-fluid">
+          <div class="float-right">
+            <a href="#" class="link-btn-plain">Cancel</a>
+            <button class="btn btn-white btn-width-xl">Preview</button>
+            <button class="btn btn-white btn-width-xl">Save as Draft</button>
+            <button class="btn btn-blue btn-width-xl">Save</button>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>    
 
  
 <?php $this->stop() ?>
