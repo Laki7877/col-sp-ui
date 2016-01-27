dd815f14152c3b6fad630964b0ef2ce1ab94fb78
fangbinc <pam.pipuch@gmail.com>
2016-01-27 22:23:18 +0700

    [Login] add modal change password

--- templates/components/header.php
+++ templates/components/header.php
@@ -6,7 +6,7 @@
         <span>user1@lara.co.th</span>
         <i class="fa fa-angle-down" data-container="body" data-html="true" data-toggle="popover" data-placement="bottom" data-content="
                   <div class='text-nowrap'><a href='#'>View Shop Profile</a></div>
-                  <div class='text-nowrap'><a href='#'>Change Password</a></div>
+                  <div class='text-nowrap'><a href='#' data-toggle='modal' data-target='#change-password'>Change Password</a></div>
                   <div class='text-nowrap'><a href='#'>Logout</a></div>
                   "></i>
     </span>
@@ -15,6 +15,6 @@
         <i class="fa fa-angle-down"></i>
     </span>
 
-    <? $this->insert('components/modal-guideline', ['id' => 'image-guideline', 'header' => 'Image Style Guideline']) ?>
+    <? $this->insert('components/modal-change-password', ['id' => 'change-password', 'header' => 'Change password']) ?>
 
 </div>
