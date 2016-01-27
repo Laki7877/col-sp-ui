00bbe658bd18ee9d7ed403d72da48638961c79ef
fangbinc <pam.pipuch@gmail.com>
2016-01-27 23:09:19 +0700

    [Header] fix layout change password modal

--- templates/components/modal-change-password.php
+++ templates/components/modal-change-password.php
@@ -1,5 +1,5 @@
 <div class="modal fade modal-custom" tabindex="-1" role="dialog" id="<?= $id ?>">
-  <div class="modal-dialog forget-modal-width">
+  <div class="modal-dialog change-password-modal">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
@@ -9,9 +9,20 @@
         <form class="ah-form margin-top-20">
           <div class="row">
             <div class="col-xs-12">
-              <div>Please check your email for your intitial password or contact</div> 
-              Central administration team at 02-123-4567 to request a
-              new one.</div>
+              
+              <!-- <div class="form-section"> -->
+                <div class="form-section-content">
+                  <? $this->insert('components/forms/password-field', ["size" => "small", "label" => "Old Password", "label_class" => "required", 'font_class' => 'hide-component']) ?>                  
+                  <? $this->insert('components/forms/password-field', ["size" => "small", "label" => "New Password", "label_class" => "required", 'font_class' => 'hide-component', 'form_group_class' => 'margin-top-30']) ?>
+                  <? $this->insert('components/forms/password-field', ["size" => "small", "label" => "Confirm Password", "label_class" => "required", 'font_class' => 'hide-component']) ?>
+                </div>
+              <!-- </div> -->
+              <div class="container-fluid no-padding margin-top-20">
+                  <div class="float-right">
+                    <a href="#" class="link-btn-plain" data-dismiss="modal">Cancel</a>
+                    <button class="btn btn-blue btn-width-xl">Save</button>
+                  </div>
+              </div>
             </div>
           </div>
         </form>
