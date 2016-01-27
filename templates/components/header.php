12ed96926d450d925892ca1629154013a7ce59dc
fangbinc <pam.pipuch@gmail.com>
2016-01-27 16:29:39 +0700

    [Header] Add link modal to header

--- templates/components/header.php
+++ templates/components/header.php
@@ -4,10 +4,17 @@
     </span>
     <span class="header-font-detail">
         <span>user1@lara.co.th</span>
-        <i class="fa fa-angle-down"></i>
+        <i class="fa fa-angle-down" data-container="body" data-html="true" data-toggle="popover" data-placement="bottom" data-content="
+                  <div class='text-nowrap'><a href='#'>View Shop Profile</a></div>
+                  <div class='text-nowrap'><a href='#'>Change Password</a></div>
+                  <div class='text-nowrap'><a href='#'>Logout</a></div>
+                  "></i>
     </span>
     <span class="header-font-detail margin-right-20">
         <span>View your store</span>
         <i class="fa fa-angle-down"></i>
     </span>
+
+    <? $this->insert('components/modal-guideline', ['id' => 'image-guideline', 'header' => 'Image Style Guideline']) ?>
+
 </div>
