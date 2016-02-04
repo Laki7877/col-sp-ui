a4fd64593437730804084ceee0031571b427c1a7
fangbinc <pam.pipuch@gmail.com>
2016-02-04 20:00:05 +0700

    [Product] fix-change: change sidebar to be simple

--- templates/product_list.php
+++ templates/product_list.php
@@ -1,6 +1,6 @@
 <?php
 
-$this->layout('layouts/page-with-sidebar-admin', ['title' => 'Administration System'])
+$this->layout('layouts/page-with-sidebar', ['title' => 'Administration System'])
 ?>
 
 <?php $this->start('page-body') ?>
