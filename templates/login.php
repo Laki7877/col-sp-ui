9184efbc2801c3e20fa5f1e4209c7b2a842a0c28
Natt Phenjati <nattphenjati@gmail.com>
2016-02-04 11:55:43 +0700

    update login.php style

--- templates/login.php
+++ templates/login.php
@@ -10,32 +10,31 @@
     <form ng-submit="doLogin()">
 
     <div class="form-login">
-        <div ng-show="alert.show" uib-alert template-url="common/alert" type="{{alert.type}}" close="alert.close()">
+        
+        <div ng-cloak ng-show="alert.show" uib-alert template-url="common/alert" type="{{alert.type}}" close="alert.close()">
             {{alert.message}}
         </div>
-        <div ng-show="!loading">
-          <fieldset class="form-group">
-            <label for="formGroupExampleInput">Username:</label>
-            <input type="text" class="form-control" ng-model="user" id="formGroupExampleInput">
-          </fieldset>
-          <fieldset class="form-group margin-bottom-30">
-            <label for="formGroupExampleInput2">Password:</label>
-            <input type="password" class="form-control" ng-model="pass" id="formGroupExampleInput2">
-          </fieldset>
-          <fieldset class="form-group">
-            <button type="submit" class="btn btn-blue btn-100">Login</button>
-          </fieldset>
-          <fieldset class="form-group text-center">
-            <a href="" data-toggle="modal" data-target="#forget-password">Forget password?</a>
-          </fieldset>
-        </div>
-         <div class="row" ng-show="loading">
-            <div class="col-md-4"></div>
-            <div class="col-md-4">
-                 <img src="/assets/img/loader.gif" width="55"/>
-            </div>  
-            <div class="col-md-4"></div>
-         </div>
+        <!-- <div ng-show="loading" class="panel panel-default login-loading">
+          <div class="panel-body">
+            <img src="/assets/img/loader.gif" width="55"/>Loading
+          </div>
+        </div> -->
+          
+        <fieldset class="form-group">
+          <label for="formGroupExampleInput">Username:</label>
+          <input type="text" class="form-control" ng-model="user" id="formGroupExampleInput">
+        </fieldset>
+        <fieldset class="form-group margin-bottom-40">
+          <label for="formGroupExampleInput2">Password:</label>
+          <input type="password" class="form-control" ng-model="pass" id="formGroupExampleInput2">
+        </fieldset>
+        <fieldset class="form-group">
+          <button type="submit" class="btn btn-blue btn-100">Login<span class="login-loading" ng-cloak ng-show="loading"><i class="fa fa-spinner fa-spin" ></i></span></button>
+        </fieldset>
+        <fieldset class="form-group text-center">
+          <a href="" data-toggle="modal" data-target="#forget-password">Forget password?</a>
+        </fieldset>
+         
     </div>
     </form>
 
