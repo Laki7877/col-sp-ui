4b1446495d411ffc9b2b12d844894c403c179afc
fangbinc <pam.pipuch@gmail.com>
2016-02-06 21:31:08 +0700

    [Product Approval] fix radio, add placeholder to textarea

--- templates/components/forms/multiple-radio.php
+++ templates/components/forms/multiple-radio.php
@@ -1,9 +1,13 @@
+<?
+$inputSize = "width-field-normal";
+if (isset($size)) $inputSize = "width-field-$size";
+?>
 <div class="form-group <?=$form_group_class?>">
 	<div class="width-label"><label class="control-label"><?= $label ?></label></div>
-	<div class="width-field-normal">
+	<div class="<?=$inputSize?>">
 		<div class="radio multiple-radio">
-			<? foreach ($choices as $choice): ?>
-				<label><input type="radio" name="todo--change-name" class="<?= $input_class; ?>"><?=$choice?></label>
+			<? foreach ($choices as $key=>$choice): ?>
+				<label class="<?=$label_class[$key]?>"><input type="radio" name="todo--change-name" class="<?= $input_class; ?>"><?=$choice?></label>
 			<? endforeach ?>
 		</div>
 	</div>
