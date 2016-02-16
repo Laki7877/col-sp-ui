fb01646a99d1788587ba6b6a7f5bc4f8f9acf66b
Pat Sabpisal <ssabpisa@me.com>
2016-02-16 23:43:56 +0700

    Fixed UI

--- templates/partials/create_coupon_information.php
+++ templates/partials/create_coupon_information.php
@@ -9,14 +9,30 @@
 					<? $this->insert('components/forms/input-text-with-label', ["label" => "Coupon Code", "label_class" => "required", "size" => "large"]) ?>
 					<? $this->insert('components/forms/input-text-with-label', ["label" => "Start Date", "label_class" => "required", "size" => "large", "input_class" => "input-icon-calendar"]) ?>
 					<? $this->insert('components/forms/input-text-with-label', ["label" => "Expire Date", "label_class" => "required", "size" => "large", "input_class" => "input-icon-calendar"]) ?>
-					<? $this->insert('components/forms/dropdown-with-label', ["label" => "Status", "label_class" => "required", 'options' => ['Active', 'Inactive']]) ?>
+				
+                    
+                     <div nc-template="common/input/form-group-with-label" nc-label="Status">
+                        <div class="ah-select2-dropdown">
+                            <select ng-model="p" class="form-control" ng-init="p = {text:'Active', value: 'AT'}" 
+                            ng-options="i as i.text for i in [{ text: 'Active', value: 'AT'},{ text: 'Inactive', value: 'IA'}] track by i.value" required>
+                            </select>
+                        </div>
+                    </div>
+                    
 				</div>
 			</div>
 			<div class="form-section">
 				<div class="form-section-header"><h2>Action</h2></div>
 				<div class="form-section-content">
-					<? $this->insert('components/forms/dropdown-with-label', ["label" => "Action", "label_class" => "required", 'options' => ['-- Select Action --', 'Discount by percent', 'Discount by amount']]) ?>
-					<? $this->insert('components/forms/input-text-with-label', ["label" => "Discount Amount", "hint" => "Example: 50 or 75", "label_class" => "required"]) ?>
+
+			          <div nc-template="common/input/form-group-with-label" nc-label="Status">
+                        <div class="ah-select2-dropdown">
+                            <select ng-model="p" class="form-control" ng-init="p = {value: 'PERCENT'}" ng-options="i as i.text for i in [{ text: 'Discount by percent', value: 'PERCENT'},{ text: 'Discount by amount', value: 'AMOUNT'}] track by i.value" required>
+                            </select>
+                        </div>
+                    </div>
+            
+            		<? $this->insert('components/forms/input-text-with-label', ["label" => "Discount Amount", "hint" => "Example: 50 or 75", "label_class" => "required"]) ?>
 					<? $this->insert('components/forms/input-text-with-label', ["label" => "Maximum Amount", 'right_hint' => 'THB']) ?>
 				</div>
 			</div>
