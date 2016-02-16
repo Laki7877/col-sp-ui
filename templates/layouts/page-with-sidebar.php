f057063d818841031866ba2569c8d3bb4b8d8463
fangbinc <pam.pipuch@gmail.com>
2016-02-16 15:18:28 +0700

    [Admin] add page orders

--- templates/layouts/page-with-sidebar.php
+++ templates/layouts/page-with-sidebar.php
@@ -7,6 +7,12 @@
     
     <?php $this->insert('components/sidebar-nav', ['itemActive' => 'Home']) ?>
     
+    <ul class="sub-sidebar" style="display: none;" id="sub-sidebar-orders">
+        <li class="sub-sidebar-header">Orders</li>
+        <li class="item active margin-top-20"><a href="?p=seller_orders">View</a></li>
+        <li class="item"><a href="?p=seller_return">Return</a></li>
+    </ul>
+
     <ul class="sub-sidebar" style="display: none;" id="sub-sidebar-product">
         <li class="sub-sidebar-header">Product</li>
         <li class="item active margin-top-20"><a href="/?p=index">View</a></li>
