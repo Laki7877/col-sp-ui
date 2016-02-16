e36de4846729cf22cd3ff67df2732b64724e06d7
fangbinc <pam.pipuch@gmail.com>
2016-02-16 17:10:07 +0700

    [Admin] Order page add table and other components

--- templates/seller_orders.php
+++ templates/seller_orders.php
@@ -2,44 +2,24 @@
 
 <?php $this->start('page-body') ?>
 	<div>
+    <? $this->insert('components/page-title-with-buttons', ['text' => 'Orders',
+        'buttons' => [
+          ]
+      ]) 
+    ?>
 
-    <div class="page-header with-border">
-        <h1 class="float-left page-header-title">Orders</h1>
-        <span class="float-right page-header-action">
-            <a href="/?p=seller_export_products" class="btn margin-left-10 btn-white btn-width-xl">
-              <span class="">Export All</span>
-            </a>
-
-            <div class="btn-group margin-left-10">
-              <button type="button" class="btn btn-white dropdown-toggle btn-width-xl" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Import <span class="caret"></span>
-              </button>
-              <ul class="dropdown-menu">
-                <li><a href="/?p=seller_import_products">Add New Products</a></li>
-                <li><a href="/?p=seller_update_products">Update Existing Products</a></li>
-              </ul>
-            </div>
-            <a href="/?p=global_category" data-toggle="modal" class="btn margin-left-10 btn-blue btn-width-xl">
-              <span class="">Add Product</span>
-            </a>
-        </span>
-    </div>
-
-    <? $this->insert('components/search-section', ['actions' => ['Export Products', 'Delete Products', 'Hide Products', 'Show Products', 'Publish Products']]) ?>
+    <? $this->insert('components/search-section', ['serach_placeholder' => 'Search for Order ID, Customer Name' , 'actions' => ['Export Products', 'Delete Products', 'Hide Products', 'Show Products', 'Publish Products'], 'optional_class' => 'hide-component']) ?>
     <div class="filter-section">
       <div class="filter-container">
         <span>Filters:</span>
         <a class="filter-first-option filter-active">All</a>
-        <a class="filter-seperator">Approved</a>
-        <a class="filter-seperator">Not Approved</a>
-        <a class="filter-seperator">Wait for Approval</a>
-        <a class="filter-seperator">Not Approved</a>
-
-        <div class="filter-checkbox">
-          <input type="checkbox" aria-label="Checkbox for following text input">
-          Show Online/Offline Status
-        </div>
-
+        <a class="filter-seperator">Payment Pending</a>
+        <a class="filter-seperator">Payment Confirmed</a>
+        <a class="filter-seperator">Preparing</a>
+        <a class="filter-seperator">Ready to ship</a>
+        <a class="filter-seperator">Shipping</a>
+        <a class="filter-seperator">Delivered</a>
+        <a class="filter-seperator">Canceled</a>
       </div>
     </div>
     <div class="table-section">
@@ -49,26 +29,34 @@
             <th class="checkbox-column">
                 <input type="checkbox" aria-label="Checkbox for following text input"> 
             </th>
-            <th class="display-column"></th>
-            <th>
-              <a class="header-link" href="#"><span>Product Name</span></a>
+            <th class="modified-column">
+              <a class="header-link" href="#"><span class="active-underline">Date</span></a>
               <i class="fa fa-caret-down color-grey">
             </th>
-            <th class="price-column">
-              <a class="header-link" href="#"><span>Price</span></a>
-              <i class="fa fa-caret-up color-grey">
+            <th class="modified-column">
+              <a class="header-link" href="#"><span>Order ID</span></a>
+              <i class="fa fa-caret-down color-grey">
             </th>
-            <th><a class="header-link" href="#"><span>Info.</span></a></th>
-            <th><a class="header-link" href="#"><span>Image</span></a></th>
-            <th class="status-column">
+            <th>
+              <a class="header-link" href="#"><span>Customer Name</span></a>
+              <i class="fa fa-caret-down color-grey">
+            </th>
+            <th class="width_100">
+              <a class="header-link" href="#"><span>Total Price</span></a>
+            </th>
+            <th class="width_100">
+              <a class="header-link" href="#">
+                <span>Carrier</span>
+              </a>
+            </th>
+            <th class="width_200">
               <a class="header-link" href="#"><span>Status</span></a>
               <i class="fa fa-caret-down color-grey">
             </th>
-            <th class="visible-column"><a class="header-link" href="#"><span>Visible</span></a></th>
-            <th class="action-column"><a class="header-link" href="#"><span>Action</span></a></th> 
-            <th class="modified-column">
-              <a class="header-link" href="#"><span class="active-underline">Modified</span></a>
-              <i class="fa fa-caret-up">
+            <th class="width_200">
+              <a class="header-link" href="#">
+                <span>Action</span>
+              </a>
             </th>
           </tr>
         </thead>
@@ -77,28 +65,22 @@
             <td class="checkbox-column">
               <input type="checkbox" aria-label="Checkbox for following text input"> 
             </td>
-            <td class="display-column">
-              <img class="logo-img" src="<?= $this->asset('/assets/img/img40.png') ?>" />
+            <td class="modified-column">
+              14/12/15
             </td>
-            <td class="column-text-ellipsis"><a href="#">Neleus Men's Slim Fit Long Sleave with Wing on the earth ground</a></td>
-            <td class="price-column">9,000,000</td>
-            <td class="info-column">
-              <i class="fa fa-check color-green icon-size-18px"></i>
-            </td>
-            <td class="image-column">
-              <i class="fa fa-check color-green icon-size-18px"></i>
-            </td>
-            <td class="status-column">
-              <span class="color-red">
-                <i class="fa fa-ban"></i>
-                Not Approved
+            <td class="modified-column"><a href="#">IV002321</a></td>
+            <td class="column-text-ellipsis ">John Bravo</td>
+            <td class="width_100">฿400.00</td>
+            <td class="width_100">Kerry</td>
+            <td class="width_200">
+              <span class="color-grey">
+                <i class="fa fa-check-circle-o"></i>
+                Payment Waiting
               </span>
             </td>
-            <td class="visible-column">
-              <i class="fa fa-eye-slash color-grey eye-icon"></i>
-            </td>
-            <td class="action-column">
-              <span class="text-align-center popover-gear">
+            <td class="width_200">
+                <button disabled class="btn btn-white btn-width-xl margin-right-15">Acknowledge</button>
+                <span class="text-align-center popover-gear">
                 <i class="fa fa-gear color-dark-grey icon-size-20"></i>
                 <i class="fa fa-caret-down color-dark-grey" data-container="body" data-html="true" data-toggle="popover" data-placement="bottom" data-content="
                   <div><a href='#' data-toggle='modal' data-target='#local-category-detail'>View / Edit</a></div>
@@ -107,39 +89,56 @@
                   "></i>  
               </span>
             </td>
+          </tr>
+          <tr>
+            <td class="checkbox-column">
+              <input type="checkbox" aria-label="Checkbox for following text input"> 
+            </td>
             <td class="modified-column">
               14/12/15
+            </td>
+            <td class="modified-column"><a href="#">IV002321</a></td>
+            <td class="column-text-ellipsis ">John Bravo</td>
+            <td class="width_100">฿400.00</td>
+            <td class="width_100">Kerry</td>
+            <td class="width_200">
+              <span class="color-grey">
+                <i class="fa fa-check-circle-o"></i>
+                Payment Confirmed
+              </span>
+            </td>
+            <td class="width_200">
+                <button class="btn btn-white btn-width-xl margin-right-15">Acknowledge</button>
+                <span class="text-align-center popover-gear">
+                <i class="fa fa-gear color-dark-grey icon-size-20"></i>
+                <i class="fa fa-caret-down color-dark-grey" data-container="body" data-html="true" data-toggle="popover" data-placement="bottom" data-content="
+                  <div><a href='#' data-toggle='modal' data-target='#local-category-detail'>View / Edit</a></div>
+                  <div><a href='#'>View Products</a></div>
+                  <div><a href='#'>Delete</a></div>
+                  "></i>  
+              </span>
             </td>
           </tr>
           <tr>
             <td class="checkbox-column">
               <input type="checkbox" aria-label="Checkbox for following text input"> 
             </td>
-            <td class="display-column">
-              <img class="logo-img" src="<?= $this->asset('/assets/img/img40.png') ?>" />
+            <td class="modified-column">
+              14/12/15
             </td>
-            <td><a href="#">Jeansian Men's Slim Fit Long</a></td>
-            <td class="price-column">
-              <div>8,999.99-9,999.99</div>
-              <div>(99 variants)</div>
-            </td>
-            <td class="info-column">
-              <i class="fa fa-check color-green icon-size-18px"></i>
-            </td>
-            <td class="image-column">
-              <i class="fa fa-check color-green icon-size-18px"></i> 
-            </td>
-            <td class="status-column">
+            <td class="modified-column"><a href="#">IV002321</a></td>
+            <td class="column-text-ellipsis ">John Bravo</td>
+            <td class="width_100">฿400.00</td>
+            <td class="width_100">Kerry</td>
+            <td class="width_200">
               <span class="color-yellow">
-                <i class="fa fa-clock-o"></i>
-                  Wait for Approval
+                <i class="fa fa-check-circle-o"></i>
+                Prepairing
               </span>
             </td>
-            <td class="visible-column">
-              <i class="fa fa-eye color-dark-grey eye-icon"></i>
-            </td>
-            <td class="action-column">
-              <span class="text-align-center popover-gear">
+            <td class="width_200">
+                <button class="btn btn-white btn-width-xl margin-right-15">Ready to Ship</button>
+                <span class="text-align-center popover-gear">
                 <i class="fa fa-gear color-dark-grey icon-size-20"></i>
                 <i class="fa fa-caret-down color-dark-grey" data-container="body" data-html="true" data-toggle="popover" data-placement="bottom" data-content="
                   <div><a href='#' data-toggle='modal' data-target='#local-category-detail'>View / Edit</a></div>
@@ -148,36 +147,27 @@
                   "></i>  
               </span>
             </td>
-            <td class="modified-column">
-              14/12/15
-            </td>
           </tr>
           <tr>
             <td class="checkbox-column">
               <input type="checkbox" aria-label="Checkbox for following text input"> 
             </td>
-            <td class="display-column">
-              <img class="logo-img" src="<?= $this->asset('/assets/img/img40.png') ?>" />
+            <td class="modified-column">
+              14/12/15
             </td>
-            <td><a href="#">Power Bank MD Tech Model B02</a></td>
-            <td class="price-column">9,000,000</td>
-            <td class="info-column">
-              <i class="fa fa-check color-green icon-size-18px"></i>
-            </td>
-            <td class="image-column">
-              <i class="fa fa-check color-green icon-size-18px"></i>
-            </td>
-            <td class="status-column">
+            <td class="modified-column"><a href="#">IV002321</a></td>
+            <td class="column-text-ellipsis ">John Bravo</td>
+            <td class="width_100">฿400.00</td>
+            <td class="width_100">Kerry</td>
+            <td class="width_200">
               <span class="color-green">
                 <i class="fa fa-check-circle-o"></i>
-                Approved
+                Ready for shipping
               </span>
             </td>
-            <td class="visible-column">
-              <i class="fa fa-eye color-dark-grey eye-icon"></i>
-            </td>
-            <td class="action-column">
-              <span class="text-align-center popover-gear">
+            <td class="width_200">
+                <button disabled class="btn btn-white btn-width-xl margin-right-15">Ready to Ship</button>
+                <span class="text-align-center popover-gear">
                 <i class="fa fa-gear color-dark-grey icon-size-20"></i>
                 <i class="fa fa-caret-down color-dark-grey" data-container="body" data-html="true" data-toggle="popover" data-placement="bottom" data-content="
                   <div><a href='#' data-toggle='modal' data-target='#local-category-detail'>View / Edit</a></div>
@@ -185,37 +175,28 @@
                   <div><a href='#'>Delete</a></div>
                   "></i>  
               </span>
-            </td>
-            <td class="modified-column">
-              09/12/15
             </td>
           </tr>
           <tr>
             <td class="checkbox-column">
               <input type="checkbox" aria-label="Checkbox for following text input"> 
             </td>
-            <td class="display-column">
-              <img class="logo-img" src="<?= $this->asset('/assets/img/img40.png') ?>" />
+            <td class="modified-column">
+              14/12/15
             </td>
-            <td><a href="#">Power Bank MD Tech Model B02</a></td>
-            <td class="price-column">1,000</td>
-            <td class="info-column">
-              <i class="fa fa-minus color-grey icon-size-18px"></i>
-            </td>
-            <td class="image-column">
-              <i class="fa fa-check color-green icon-size-18px"></i>
-            </td>
-            <td class="status-column">
-              <span class="color-grey">
-                <i class="fa fa-circle-o"></i>
-                Draft
+            <td class="modified-column"><a href="#">IV002321</a></td>
+            <td class="column-text-ellipsis ">John Bravo</td>
+            <td class="width_100">฿400.00</td>
+            <td class="width_100">Kerry</td>
+            <td class="width_200">
+              <span class="color-green">
+                <i class="fa fa-clock-o"></i>
+                Shipped
               </span>
             </td>
-            <td class="visible-column">
-              <i class="fa fa-eye color-dark-grey eye-icon"></i>
-            </td>
-            <td class="action-column">
-              <span class="text-align-center popover-gear">
+            <td class="width_200">
+                <button disabled class="btn btn-white btn-width-xl margin-right-15">Ready to Ship</button>
+                <span class="text-align-center popover-gear">
                 <i class="fa fa-gear color-dark-grey icon-size-20"></i>
                 <i class="fa fa-caret-down color-dark-grey" data-container="body" data-html="true" data-toggle="popover" data-placement="bottom" data-content="
                   <div><a href='#' data-toggle='modal' data-target='#local-category-detail'>View / Edit</a></div>
@@ -224,8 +205,63 @@
                   "></i>  
               </span>
             </td>
+          </tr>
+          <tr>
+            <td class="checkbox-column">
+              <input type="checkbox" aria-label="Checkbox for following text input"> 
+            </td>
             <td class="modified-column">
-              09/12/15
+              14/12/15
+            </td>
+            <td class="modified-column"><a href="#">IV002321</a></td>
+            <td class="column-text-ellipsis ">John Bravo</td>
+            <td class="width_100">฿400.00</td>
+            <td class="width_100">Kerry</td>
+            <td class="width_200">
+              <span class="color-green">
+                <i class="fa fa-check-circle-o"></i>
+                Delivered
+              </span>
+            </td>
+            <td class="width_200">
+                <button disabled class="btn btn-white btn-width-xl margin-right-15">Ready to Ship</button>
+                <span class="text-align-center popover-gear">
+                <i class="fa fa-gear color-dark-grey icon-size-20"></i>
+                <i class="fa fa-caret-down color-dark-grey" data-container="body" data-html="true" data-toggle="popover" data-placement="bottom" data-content="
+                  <div><a href='#' data-toggle='modal' data-target='#local-category-detail'>View / Edit</a></div>
+                  <div><a href='#'>View Products</a></div>
+                  <div><a href='#'>Delete</a></div>
+                  "></i>  
+              </span>
+            </td>
+          </tr>
+          <tr>
+            <td class="checkbox-column">
+              <input type="checkbox" aria-label="Checkbox for following text input"> 
+            </td>
+            <td class="modified-column">
+              14/12/15
+            </td>
+            <td class="modified-column"><a href="#">IV002321</a></td>
+            <td class="column-text-ellipsis ">John Bravo</td>
+            <td class="width_100">฿400.00</td>
+            <td class="width_100">Kerry</td>
+            <td class="width_200">
+              <span class="color-red">
+                <i class="fa fa-ban"></i>
+                Canceled
+              </span>
+            </td>
+            <td class="width_200">
+                <button disabled class="btn btn-white btn-width-xl margin-right-15">Acknowledge</button>
+                <span class="text-align-center popover-gear">
+                <i class="fa fa-gear color-dark-grey icon-size-20"></i>
+                <i class="fa fa-caret-down color-dark-grey" data-container="body" data-html="true" data-toggle="popover" data-placement="bottom" data-content="
+                  <div><a href='#' data-toggle='modal' data-target='#local-category-detail'>View / Edit</a></div>
+                  <div><a href='#'>View Products</a></div>
+                  <div><a href='#'>Delete</a></div>
+                  "></i>  
+              </span>
             </td>
           </tr>
         </tbody>
