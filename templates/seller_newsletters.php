260710d73f497515e37d897a8fbf73e04edb3ba1
fangbinc <pam.pipuch@gmail.com>
2016-02-19 03:24:35 +0700

    [Seller] add seller newsletters detai page

--- templates/seller_newsletters.php
+++ templates/seller_newsletters.php
@@ -33,7 +33,7 @@
               <a href="#">Harry Belafonte</a> 
             </td>
             <td class="action-column text-align-center">
-              <button class="btn btn-white btn-width-default">Read</button>
+              <button class="btn btn-white btn-width-default" data-toggle="modal" data-target="#seller_newsletter_detail_id">Read</button>
             </td>
             
           </tr>
@@ -64,6 +64,11 @@
     </div>
   </div>
 
-  <? $this->insert('components/modal_admin_newsletter_detail', ['id' => 'image-guideline', 'header' => 'Add Newsletter']) ?>
+  <? $this->insert('components/modal_seller_newsletter_detail', ['id' => 'seller_newsletter_detail_id', 'header' => 'You take my money and run away', 'date' => 'Publish Date: 05/11/15', 'author' => 'By: Admin',
+   'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique elit sit amet turpis imperdiet consectetur. Donec tincidunt lobortis arcu at condimentum. Sed ultricies lacus nisi, in imperdiet nulla tristique eu. Phasellus vel mauris condimentum magna iaculis sollicitudin vel a nulla. Morbi pharetra sed ligula ut suscipit. Aliquam in purus facilisis, mattis mauris ornare, lacinia neque. Aenean non maximus dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula enim, finibus et risus dignissim, vulputate congue massa. Duis leo libero, feugiat quis facilisis ac, mollis id purus. Phasellus tincidunt felis ac mi sodales imperdiet. Proin viverra augue at ultricies facilisis.
+
+    Donec luctus dignissim consectetur. Pellentesque id hendrerit mauris, vitae dapibus nunc. Nulla sed consequat eros. Maecenas consequat justo nec porttitor feugiat. Nullam dapibus quam lacus, eget sodales est lacinia a. Donec ac ullamcorper turpis. Nulla at dolor sed sapien vulputate sollicitudin. Suspendisse sagittis libero at nisl efficitur rutrum.
+
+    Vestibulum eu diam et ipsum volutpat pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam tempus auctor elit, id volutpat leo posuere ut. Proin id aliquam risus, id ultrices nunc. Praesent tempor eros sit amet lectus lacinia, eget scelerisque leo dignissim. In felis magna, faucibus non urna in, rutrum congue quam. Sed vel hendrerit lacus, sit amet dapibus ligula. Aliquam sed enim tellus. Integer interdum dui ultricies feugiat sodales. Fusce imperdiet elementum facilisis. In imperdiet ex at nulla interdum faucibus a a mauris.']) ?>
  
 <?php $this->stop() ?>
