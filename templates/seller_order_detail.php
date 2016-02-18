3cb89eb6dae4dd9122a4fee94e809b5c4efa0d2c
fangbinc <pam.pipuch@gmail.com>
2016-02-18 12:14:55 +0700

    [Seller] Order detail finish style table, add section cancel

--- templates/seller_order_detail.php
+++ templates/seller_order_detail.php
@@ -80,14 +80,70 @@ $this->layout('layouts/page-with-sidebar', ['title' => 'Order Detail'])
       </div>
     </div>
 
+    <div class="table-section table_order">
+      <table class="table table-curved product-list-table">
+        <thead>
+          <tr class="table-head">
+            <th>
+              <a class="header-link" href="#"><span>Product Name</span></a>
+            </th>
+            <th class="width_100 ">
+              <a class="header-link" href="#"><span>Price / Unit</span></a>
+            </th>
+            <th class="width_100 text-align-center">
+              <a class="header-link" href="#"><span>Quantity</span></a>
+            </th>
+            <th class="width_100 text-align-center">
+              <a class="header-link" href="#"><span>Total Price</span></a>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="column-text-ellipsis"><a href="#">Neleus Men's Slim Fit Long Sleave with Wing on the earth ground</a></td>
+            <td class="width_100 ">฿400.00</td>
+            <td class="width_100 text-align-center">1</td>
+            <td class="width_100 text-align-center">฿400.00</td>
+          </tr>
+          <tr>
+            <td class="column-text-ellipsis"><a href="#">Power Bank MD Tech Model B01</a></td>
+            <td class="width_100 ">฿200.00</td>
+            <td class="width_100 text-align-center">2</td>
+            <td class="width_100 text-align-center">฿800.00</td>
+          </tr>
+          <tr>
+            <td class="">Sub Total</td>
+            <td class="width_100 "></td>
+            <td class="width_100 text-align-center"></td>
+            <td class="width_100 text-align-center">฿1,200.00</td>
+          </tr>
+          <tr class="color-red">
+            <td class="">Discount: Father Day Coupon</td>
+            <td class="width_100 "></td>
+            <td class="width_100 text-align-center"></td>
+            <td class="width_100 text-align-center">- ฿200.00</td>
+          </tr>
+          <tr class="color-red">
+            <td class="">Discount: Winter Sale</td>
+            <td class="width_100 "></td>
+            <td class="width_100 text-align-center"></td>
+            <td class="width_100 text-align-center">- ฿120.00</td>
+          </tr>
+          <tr class="background_light_yellow ">
+            <td class="">Total Order Price</td>
+            <td class="width_100 "></td>
+            <td class="width_100 text-align-center"></td>
+            <td class="width_100 text-align-center">฿880.00</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
     <div class="add-product-body">
       <form class="ah-form sticky-mainform-action">
         <div class="tab-content">
           <div role="tabpanel" class="tab-pane margin-top-20 active" id="information">
-            <? $this->insert('partials/create_coupon_information') ?>
-          </div>
-          <div role="tabpanel" class="tab-pane margin-top-20" id="conditions">
-            <? $this->insert('partials/coupon_condition') ?>
+            <? $this->insert('partials/order_cancel') ?>
           </div>
         </div>
         <div class="add-product-form-action main-form-action full-width-row">
