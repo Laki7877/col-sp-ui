6e8fb1979a8d339c52faf00f08417c301bb1f15a
Natt Phenjati <nattphenjati@gmail.com>
2016-02-23 11:23:44 +0700

    link shop profile setting

--- templates/components/sidebar-nav.php
+++ templates/components/sidebar-nav.php
@@ -4,8 +4,8 @@
         [ "name" => "Orders", "icon" => "fa-inbox"],
         [ "name" => "Products", "icon" => "fa-tag", "sub_sidebar" => "#sub-sidebar-product", "url" => "/products" ],
         [ "name" => "Inventory", "icon" => "fa-archive", "sub_sidebar" => "#sub-sidebar-inventory"],
-        [ "name" => "Promotion", "icon" => "fa-bookmark" ],
-        [ "name" => "Shop Setting", "icon" => "fa-sliders" ],
+        [ "name" => "Promotion", "icon" => "fa-bookmark", "sub_sidebar" => "#sub-sidebar-promotion" ],
+        [ "name" => "Shop Setting", "icon" => "fa-sliders", "sub_sidebar" => "#sub-sidebar-shopsetting" ],
         [ "name" => "Report", "icon" => "fa-line-chart" ],
         [ "name" => "Account", "icon" => "fa-gear", "sub_sidebar" => "#sub-sidebar-account" ],
     ];
--- templates/layouts/page-with-sidebar.php
+++ templates/layouts/page-with-sidebar.php
@@ -4,9 +4,9 @@
 	<div class="logo-img-wrapper">
 		<img class="logo-img" src="<?= $this->asset('/assets/img/seller_logo.png') ?>" />
 	</div>
-    
+
     <?php $this->insert('components/sidebar-nav') ?>
-    
+
     <ul class="sub-sidebar" style="display: none;" id="sub-sidebar-product">
         <li class="sub-sidebar-header">Product</li>
         <li ng-class="activeUrl('/products')" class="item margin-top-20"><a href="/products">View</a></li>
@@ -21,14 +21,17 @@
     <ul class="sub-sidebar" style="display: none;" id="sub-sidebar-inventory">
         <li class="sub-sidebar-header">Inventory</li>
         <li ng-class="activeUrl('/inventory')" class="item margin-top-20"><a href="/inventory">View</a></li>
-        <!-- <li class="item"><a href="/?p=add_product">Add</a></li> -->
-        <!-- <li class="item"><a href="/?p=import_product">Import</a></li> -->
-        <!-- <li class="item"><a href="/?p=index">Export</a></li> -->
     </ul>
 
     <ul class="sub-sidebar" style="display: none;" id="sub-sidebar-promotion">
         <li class="sub-sidebar-header">Promotion</li>
-        <li class="item active margin-top-20"><a href="/?p=seller_coupons">Coupons</a></li>
+        <li class="item active margin-top-20"><a href="#">Coupons</a></li>
+    </ul>
+
+		<ul class="sub-sidebar" style="display: none;" id="sub-sidebar-shopsetting">
+        <li class="sub-sidebar-header">Shop Setting</li>
+        <li ng-class="activeUrl('/shops/settings')" class="item margin-top-20"><a href="/shops/settings">Shop Profile</a></li>
+				<li class="item"><a href="#">Shop Appearance</a></li>
     </ul>
 
     <ul class="sub-sidebar" style="display: none;" id="sub-sidebar-account">
