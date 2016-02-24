de6a9c31abcc974a8a5212c628d601ecec49073f
Pat Sabpisal <ssabpisa@me.com>
2016-02-24 17:08:59 +0700

    link fix

--- templates/components/page-title-with-buttons.php
+++ templates/components/page-title-with-buttons.php
@@ -2,7 +2,7 @@
 we should just make it modular by creating initializer -->
 <div class="page-header with-border">
     <h1 class="float-left page-header-title"><?= $text ?></h1>
-    <form action="products/export" method="post">
+    <form action="/products/export" method="post">
     <span class="float-right page-header-action">
           <input type="hidden" name="selected_products[]" ng-repeat="(prId,tf) in checkBoxCache"
           value="{{ prId }}" ng-if="tf === true"/>
