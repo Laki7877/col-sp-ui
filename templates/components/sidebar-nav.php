75cb31094c37c2f56e6d0823dfe61c7f59d68c09
Natt Phenjati <nattphenjati@gmail.com>
2016-02-24 14:37:58 +0700

    add like to side menu - inventory

--- templates/components/sidebar-nav.php
+++ templates/components/sidebar-nav.php
@@ -3,7 +3,7 @@
         [ "name" => "Home", "icon" => "fa-home"],
         [ "name" => "Orders", "icon" => "fa-inbox"],
         [ "name" => "Products", "icon" => "fa-tag", "sub_sidebar" => "#sub-sidebar-product", "url" => "/products", "link" =>"/products" ],
-        [ "name" => "Inventory", "icon" => "fa-archive", "sub_sidebar" => "#sub-sidebar-inventory"],
+        [ "name" => "Inventory", "icon" => "fa-archive", "sub_sidebar" => "#sub-sidebar-inventory", "url" => "/inventory", "link" =>"/inventory"],
         [ "name" => "Promotion", "icon" => "fa-bookmark", "sub_sidebar" => "#sub-sidebar-promotion" ],
         [ "name" => "Shop Setting", "icon" => "fa-sliders", "sub_sidebar" => "#sub-sidebar-shopsetting", "link" =>"/shops/settings" ],
         [ "name" => "Report", "icon" => "fa-line-chart" ],
