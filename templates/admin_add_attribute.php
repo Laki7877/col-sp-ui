b4f0a03affe796c95ab0a32e68ca0c5b93d3cb7c
Natt Phenjati <nattphenjati@gmail.com>
2016-02-25 14:38:17 +0700

    Change trash can size

--- templates/admin_add_attribute.php
+++ templates/admin_add_attribute.php
@@ -171,7 +171,7 @@
                               <span ng-show="form['ltChoiceEn' + $index].$error.pattern">Only English allowed</span>
                           </div>
                         </div>
-                        <i ng-if="$index > 0" class="clickable fa fa-trash fa-2x margin-left-10 color-grey margin-top-5" ng-click="formData.LT.AttributeValues.splice($index,1)"></i>
+                        <i ng-if="$index > 0" class="clickable fa fa-trash margin-left-10 color-dark-grey margin-top-10" ng-click="formData.LT.AttributeValues.splice($index,1)"></i>
                       </div>
                     </div>
                   </div>
