02277e2768da1fc672ff49e8fb86c6c1f3da95f2
Poon Wu <poon.wuthi@gmail.com>
2016-02-26 22:01:43 +0700

    UI Fix

--- angular/template-options/adminShopAccountForm.js
+++ angular/template-options/adminShopAccountForm.js
@@ -3,10 +3,6 @@ module.exports = {
         inputSize: 'large'
 	},
 	BankAccountName: {
-        inputSize: 'large',
-        hint: {
-            show: true,
-            message: 'Please contact COL if you wish to change your financial detail'
-        }
+        inputSize: 'large'
 	}
 }
