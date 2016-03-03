cc2590f4a1b92c7812ad314da187a02973362fbf
fangbinc <pam.pipuch@gmail.com>
2016-03-03 16:19:53 +0700

    [Seller] Add onboarding routing to seller-sidebar

---
+++ angular/controllers/sellerOnboarding.js
@@ -0,0 +1,5 @@
+module.exports = function($scope, $controller, config) {
+	'ngInject';
+	//Inherit from parent
+	$controller();
+};
--- angular/route.js
+++ angular/route.js
@@ -57,7 +57,7 @@ function generateRouteArray(obj) {
 
 var seller = {
 	'Home|fa-home': {
-
+		'Onboarding': '/onboarding'
 	},
 	'Orders|fa-inbox': {
 
