93f29b5e58dff8100fb1f0fccf14e11033ef485e
Natt Phenjati <nattphenjati@gmail.com>
2016-03-07 11:25:28 +0700

    Update Header

--- templates/components/header.php
+++ templates/components/header.php
@@ -21,7 +21,7 @@
           <i class="fa fa-angle-down"></i>
           </a>
           <ul class="dropdown-menu" ng-if="Profile.Shop">
-            <li><a href="#">Status: <span class="{{asShopStatus(Profile.Shop.Status).color}}">{{asShopStatus(Profile.Shop.Status).name}}</span></a></li>
+            <li><a href="/shops/settings">Status: <span class="{{asShopStatus(Profile.Shop.Status).color}}">{{asShopStatus(Profile.Shop.Status).name}}</span></a></li>
             <li role="separator" class="divider"></li>
             <li><a href="http://www.central.co.th" target="_blank">View Storefront</a></li>
             <li><a href="/shops/settings">View Shop Profile</a></li>
