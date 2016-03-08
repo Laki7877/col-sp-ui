dcfe2f8a186dd8ae3dc2d49250483cc724641e9f
นที พูลภักดี < >
2016-03-08 19:20:34 +0700

    add col dev

---
+++ angular/col-dev/modules/cms-cate/views/cms-cate-main.php
@@ -0,0 +1,9 @@
+<?php $this->layout('layouts/page-with-sidebar-admin', ['title' => 'Administration System']) ?>
+
+<?php $this->start('page-body') ?>
+  <div ng-controller="CMSCateController">
+
+    Hello
+
+  </div>
+<?php $this->stop() ?>
