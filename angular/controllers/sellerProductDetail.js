110158593a4a0a99251720a92f7108a3b916d972
Pat Sabpisal <ssabpisa@me.com>
2016-03-24 14:22:21 +0700

    Move dlocation of test tool

--- angular/controllers/sellerProductDetail.js
+++ angular/controllers/sellerProductDetail.js
@@ -13,11 +13,20 @@ module.exports = function($scope, $controller, storage) {
   /*
   *  Debugging Features
   */
-
+  
+  var checkpoint = "T100";
+  $scope.disableDebugLoad = false;
+  var debugfd = storage.get('save-fd-' + checkpoint);
+  if(!debugfd){
+      $scope.disableDebugLoad = true;
+  }
+  
   $scope._debugLoad = function() {
-
-    var checkpoint = "T100";
-    $scope.formData = JSON.parse(storage.get('save-fd-' + checkpoint));
+    var M = storage.get('save-fd-' + checkpoint);
+    if(!M){
+       return alert("Unable to restore saved data from local storage. Perhaps, you haven't saved.")
+    };
+    $scope.formData = JSON.parse(M);
     $scope.dataset = JSON.parse(storage.get('save-ds-' + checkpoint));
     $scope.variationFactorIndices.iterator = JSON.parse(storage.get('save-it-' + checkpoint));
     $scope.controlFlags = JSON.parse(storage.get('save-cf-' + checkpoint));
@@ -25,11 +34,11 @@ module.exports = function($scope, $controller, storage) {
     console.log($scope.formData, "loaded");
     $scope.variantPtr = $scope.formData.MasterVariant;
     $scope.variantPtr.Images = $scope.formData.MasterVariant.Images;
+    $scope.alert.success('<strong>Testing Kit</strong> Product data restored.');
   }
 
   $scope._debugSave = function() {
 
-    var checkpoint = "T100";
     $scope.formData.GlobalCategories.forEach(function(gc) {
       if (gc == null) return;
       delete gc.nodes;
@@ -40,10 +49,12 @@ module.exports = function($scope, $controller, storage) {
     delete $scope.dataset.LocalCategories;
     console.log($scope.dataset, $scope.variationFactorIndices);
 
-    storage.put('save-cf-' + checkpoint, JSON.stringify($scope.controlFlags));
-    storage.put('save-fd-' + checkpoint, JSON.stringify($scope.formData));
-    storage.put('save-ds-' + checkpoint, JSON.stringify($scope.dataset));
-    storage.put('save-it-' + checkpoint, JSON.stringify($scope.variationFactorIndices.iterator));
+    storage.put('save-cf-' + checkpoint, JSON.stringify($scope.controlFlags), true);
+    storage.put('save-fd-' + checkpoint, JSON.stringify($scope.formData), true);
+    storage.put('save-ds-' + checkpoint, JSON.stringify($scope.dataset), true);
+    storage.put('save-it-' + checkpoint, JSON.stringify($scope.variationFactorIndices.iterator), true);
+    $scope.disableDebugLoad = false;
+    $scope.alert.success('<strong>Testing Kit</strong> Product data saved to your local storage.');
   };
 
 };
--- templates/add_product.php
+++ templates/add_product.php
@@ -9,8 +9,6 @@ $this->layout('layouts/page-with-sidebar', ['title' => 'Seller Portal - Product'
                     <button type="submit" class="btn btn-blue btn-width-xl" ng-click="saveAsIs()">Save</button>
                 </div>
                 <div class="page-actions float-right" ng-if="!_loading.state && !adminMode && formData.Status != 'AP'">
-                    <button ng-click="_debugLoad()" class="btn btn-xs btn-success"><i class="glyphicon glyphicon-folder-open"></i></button>
-				    <button ng-click="_debugSave()" class="btn btn-xs btn-success"><i class="glyphicon glyphicon-floppy-disk"></i></button>
                     <button ng-show="formData.Status != 'WA'" class="btn btn-white btn-width-xl" ng-click="preview()">Preview</button>
                     <button ng-show="formData.Status != 'WA'" class="btn btn-white btn-width-xl" type="submit" ng-click="publish('DF')">Save as Draft</button>
                     <button ng-show="formData.Status != 'WA'" type="submit" class="btn btn-blue btn-width-xl" ng-click="prePublishWA()">Publish</button>
@@ -22,12 +20,21 @@ $this->layout('layouts/page-with-sidebar', ['title' => 'Seller Portal - Product'
 </script>
 
 <div ng-controller="SellerProductDetailCtrl" ng-init='init(<?= json_encode($viewBag) ?>)'>
+
        <nc-alert nc-model="devAlert"></nc-alert>
        <nc-alert nc-model="alert"></nc-alert>
        <nc-alert nc-model="adminAlert"></nc-alert>
-
+       
+       <div style="padding-bottom: 10px" ng-show="formData.ProductId">
+               <button ng-click="_debugLoad()" class="btn btn-xs" ng-disabled="disableDebugLoad">
+                    <span class="glyphicon glyphicon-asterisk"></span> Load Cache
+                </button>
+                <button ng-click="_debugSave()" class="btn btn-xs">
+                    <span class="glyphicon glyphicon-asterisk"></span> Save Cache
+                </button>
+       </div>
+       
        <div ap-component="ap/form-product-add"></div>
-
 </div>
 
 <?php $this->stop() ?>
