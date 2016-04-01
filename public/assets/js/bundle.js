8fa54e78b5c1b2ed18d9aee73104564cf827c929
Pat Sabpisal <ssabpisa@me.com>
2016-04-01 17:26:05 +0700

    forgto terontu

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -10136,6 +10136,7 @@ angular.module('productDetail').controller('AbstractProductAddCtrl',
         }
 
         $scope.formData.Variants.forEach(function(variant) {
+          if(!variant.Visibility) return;
           if (variant.Images.length == 0) {
             mat.push('At least one image for variation ' + "'" + variant.text + "'");
           }
