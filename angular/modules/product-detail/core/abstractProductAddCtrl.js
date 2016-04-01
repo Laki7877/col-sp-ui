697f6e179766ddbbfe72b36fe78a01332de88f1c
Pat Sabpisal <ssabpisa@me.com>
2016-04-01 17:23:57 +0700

    Visi

--- angular/modules/product-detail/core/abstractProductAddCtrl.js
+++ angular/modules/product-detail/core/abstractProductAddCtrl.js
@@ -335,6 +335,7 @@ angular.module('productDetail').controller('AbstractProductAddCtrl',
         }
 
         $scope.formData.Variants.forEach(function(variant) {
+          if(!variant.Visibility) return;
           if (variant.Images.length == 0) {
             mat.push('At least one image for variation ' + "'" + variant.text + "'");
           }
