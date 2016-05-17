62f191490b465e9881e880d74dbf0168cb37333f
Pat Sabpisal <ssabpisa@me.com>
2016-05-17 14:26:35 +0700

    exp

--- angular/controllers/productExport.js
+++ angular/controllers/productExport.js
@@ -22,11 +22,11 @@ module.exports = function ($scope, Product, AttributeSet, NcAlert, $base64, $fil
 		});
 
 		var groupList = Object.keys($scope.availableFields);
-			for(var i = 0; i <= $scope.columnCount; i++){
+			for(var i = 0; i < $scope.columnCount; i++){
 				var dct = {};
 				var keyPerColumn = Math.ceil(groupList.length / $scope.columnCount);
 
-				for(var j = 0 ; j < keyPerColumn; j++){
+				for(var j = 0 ; j <= keyPerColumn; j++){
 					var moveKey = groupList.shift();
 					dct[moveKey] = ($scope.availableFields[moveKey]);
 				}
