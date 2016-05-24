1fb874887ff99889044c809843636114ca335841
Poon Wu <poon.wuthi@gmail.com>
2016-05-24 17:08:34 +0700

    Fixed date default for newsletter

--- angular/services/newsletterService.js
+++ angular/services/newsletterService.js
@@ -3,8 +3,18 @@ module.exports = function(common) {
 
 	service.generate = function() {
 		return {
-			VisibleShopGroup: 'AL'
+			VisibleShopGroup: 'AL',
+			PublishedDt: moment(new Date()).add(1, 'minutes')
 		};
+	};
+	service.serialize = function(data) {
+		if(!data.PublishedDt) {
+			data.PublishedDt = moment(new Date()).add(30, 'minutes');
+		}
+		if(!data.ExpiredDt) {
+			data.ExpiredDt = moment(new Date()).add(20, 'years')
+		}
+		return data;
 	};
 	service.deserialize = function(data) {
 		return _.extend(this.generate(), data);
