85ed4658194b57da4d3e32792ee6732de5e63fcd
poonwu <poon.wuthi@gmail.com>
2016-05-26 23:06:08 +0700

    Tried to fix product layout blank field

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -15029,7 +15029,7 @@ angular.module('nc')
 					}
 					else {
 						if(scope.source.Products) {
-							scope.source.Products = _.compact(scope.source.Products);
+							_.remove(scope.source.Products, _.isEmpty);
 						}
 					}
 				})
