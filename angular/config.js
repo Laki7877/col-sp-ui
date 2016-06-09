c1544ce9662314f23cfbd4cd472197577e2153d8
Natt Phenjati <nattphenjati@gmail.com>
2016-06-09 18:59:09 +0700

    config for 104

--- angular/config.js
+++ angular/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	//REST_SERVICE_BASE_URL: 'http://ahancer-colsp.azurewebsites.net/api',
-	REST_SERVICE_BASE_URL: 'http://colsp-dev.azurewebsites.net/api',
+	REST_SERVICE_BASE_URL: 'http://ahancer-colsp.azurewebsites.net/api',
+	//REST_SERVICE_BASE_URL: 'http://colsp-dev.azurewebsites.net/api',
 	//REST_SERVICE_BASE_URL: 'http://27.254.48.174/sellerportal/api',
 	//REST_SERVICE_BASE_URL: 'http://localhost:58127/api',
 	//REST_SERVICE_BASE_URL: 'http://devmkp-colspapi.cenergy.co.th/api',
